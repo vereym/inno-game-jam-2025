0f3dbcdf166663b61fb189b3a1c5d045aabffea7
reym <reym@veril.mozmail.com>
2025-11-08 18:50:27 +0100

    add diagonal movement

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 signal goal_reached
 
+enum Movement {
+    Normal,
+    Diagonal,
+}
+
 @onready var ray := $RayCast2D
+@onready var current_movement := Movement.Normal
 
 var moving := false
 var animation_speed := 10
@@ -13,13 +19,19 @@ var direction_vectors := {
     "up": Vector2.UP,
     "down": Vector2.DOWN,
 }
-
-func _ready() -> void:
-    pass
+var diagonal_vectors := {
+    "left": Vector2(0, -1),
+    "right": Vector2(0, 1),
+    "up": Vector2(1,0),
+    "down": Vector2(-1, 0),
+}
 
 func _unhandled_key_input(event: InputEvent) -> void:
     if moving:
         return
+    if event.is_action_pressed("coin_flip"):
+        current_movement = Movement.Diagonal if current_movement == Movement.Normal else Movement.Normal
+
     if event.is_action_pressed("left"): move("left")
     if event.is_action_pressed("right"): move("right")
     if event.is_action_pressed("up"): move("up")
@@ -27,13 +39,19 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 
 func move(dir):
+    var target_position: Vector2
+    if current_movement == Movement.Diagonal:
+        target_position = (direction_vectors[dir] * tile_size) + (diagonal_vectors[dir] * tile_size)
+    else:
+        target_position = direction_vectors[dir] * tile_size
+
     # check if field where we want to go is a collision
-    ray.target_position = direction_vectors[dir] * tile_size
+    ray.target_position = target_position
     ray.force_raycast_update()
     if !ray.is_colliding():
         var tween := create_tween()
         tween.tween_property(self, "position",
-            position + direction_vectors[dir] * tile_size, 1./animation_speed).set_trans(Tween.TRANS_SINE)
+            position + target_position, 1./animation_speed).set_trans(Tween.TRANS_SINE)
         moving = true
         await tween.finished
         moving = false
