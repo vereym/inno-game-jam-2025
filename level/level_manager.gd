49e8e0bfd33290fefd378679fbaac8de5e830e10
reym <reym@veril.mozmail.com>
2025-11-09 11:58:42 +0100

    add restart_level action

--- level/level_manager.gd
+++ level/level_manager.gd
@@ -1,15 +1,25 @@
 extends Node2D
 
-@onready var current_level := $Level
+signal level_changed
+
+@onready var current_level: Node2D = $Level
 
 var levels := []
 
+func _input(event):
+    if event.is_action_pressed("restart_level"):
+        _change_current_scene(current_level)
 
 func next_level():
     var next := load("res://level/level2.tscn").instantiate() as TileMapLayer
     _change_current_scene(next)
+    level_changed.emit()
 
 func _change_current_scene(scene):
     current_level.queue_free()
     current_level = scene
     add_child(current_level)
+
+func get_starting_position() -> Vector2:
+    var starting_position := current_level.find_child("StartingPosition") as Node2D
+    return starting_position.position
--- main.gd
+++ main.gd
@@ -6,3 +6,8 @@ var custom_tileset := preload("res://level/custom_tileset.tres")
 func _ready() -> void:
     %Player.tile_size = custom_tileset.tile_size.x
     %Player.goal_reached.connect(%LevelManager.next_level)
+
+    %LevelManager.level_changed.connect(
+        func():
+            %Player.position = %LevelManager.get_starting_position()
+    )
